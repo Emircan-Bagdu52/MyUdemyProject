c7f087b469b682da1f647920faff17793a797b9b
emirc <emirc@DESKTOP-39HLU0C>
2023-09-17 14:46:39 +0300

    Add project files.

---
+++ ApiConsume/HotelProject.DataAccesLayer/Concrete/Context.cs
@@ -0,0 +1,24 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccesLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-39HLU0C\\SQLEXPRESS;initial catalog=ApiDb; integrated security=true");
+        }
+        /* sol taraftaki sınıfın ismi sağ taraftaki sqlde oluşacak tablo adı*/
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
